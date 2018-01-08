b13acd2d6f13ae9247513ce0d2f57388e9d46fff
Yan Fu <Yan.Fu@autodesk.com>
2018-01-08 14:44:40 -0800

    1. Add method to make train data set and test data set chunked so that new data can be appended . 2. Add a method to update training data set and test data set with pending data

--- server/h5test.py
+++ server/h5test.py
@@ -69,27 +69,90 @@ def load_dataset():
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-'''
-def update_dataset():
+def copy_dataset():
+
+    train_dataset = h5py.File('datasets/train_happy.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    train_set_y_orig = train_set_y_orig.reshape((train_set_y_orig.shape[0], 1))
+    print(np.shape(train_set_x_orig) )
+    print(np.shape(train_set_y_orig) )
+    train_dataset.close()
+
+    test_dataset = h5py.File('datasets/test_happy.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    test_set_y_orig = test_set_y_orig.reshape((test_set_y_orig.shape[0],1))
+    test_dataset.close()
     
-    with h5py.File('datasets/train_happy.h5', 'a') as h5f:
-        h5f["train_set_x"].resize((hf["X_train"].shape[0] + X_train_data.shape[0]), axis = 0)
+    newH5fTrain = h5py.File('datasets/train_happy_chunked.h5', "w")
+
+    newH5fTrain.create_dataset("train_set_x", data = train_set_x_orig, maxshape=(None,64,64,3), chunks=True)
+    newH5fTrain.create_dataset("train_set_y", data = train_set_y_orig, maxshape=(None,1), chunks=True) 
+    train_x = newH5fTrain["train_set_x"]
+    train_y = newH5fTrain["train_set_y"]
+    #test_x[0] = image_data
+    #test_y[0] = img_label
+    print(np.shape(train_x))
+    print(np.shape(train_y))
+    newH5fTrain.close()
+    
+    newH5fTest = h5py.File('datasets/test_happy_chunked.h5', "w")
+    
+    newH5fTest.create_dataset("test_set_x", data = test_set_x_orig, maxshape=(None,64,64,3), chunks=True)
+    newH5fTest.create_dataset("test_set_y", data = test_set_y_orig, maxshape=(None,1), chunks=True) 
+    test_x = newH5fTest["test_set_x"]
+    test_y = newH5fTest["test_set_y"]
+    print(np.shape(test_x))
+    print(np.shape(test_y))
+    #print("create h5 file")
+    #print(h5f.keys() )
+    newH5fTest.close()
+    
+
+def update_dataset():
+    pendingh5f = h5py.File("datasets/pending_face_test.h5", "r")
+    pending_set_x = pendingh5f["pending_set_x"]
+    pending_set_y = pendingh5f["pending_set_y"]
+    training_ratio = 0.8
+    pending_size = np.shape(pending_set_x)[0]
+    training_size = int(training_ratio*pending_size)
+    X_train_data = pending_set_x[0:training_size,:,:,:]
+    Y_train_data = pending_set_y[0:training_size,:]
+    X_test_data = pending_set_x[training_size:,:,:,:]
+    Y_test_data = pending_set_y[training_size:,:]
+    print(np.shape(X_train_data))
+    print(np.shape(X_test_data))
+    print(np.shape(Y_train_data))
+    print(np.shape(Y_test_data))
+    pendingh5f.close()
+        
+
+    with h5py.File('datasets/train_happy_chunked.h5', 'a') as h5f:
+        h5f["train_set_x"].resize((h5f["train_set_x"].shape[0] + X_train_data.shape[0]), axis = 0)
         h5f["train_set_x"][-X_train_data.shape[0]:] = X_train_data
 
-        h5f["train_set_y"].resize((hf["pending_set_y"].shape[0] + Y_train_data.shape[0]), axis = 0)
+        h5f["train_set_y"].resize((h5f["train_set_y"].shape[0] + Y_train_data.shape[0]), axis = 0)
         h5f["train_set_y"][-Y_train_data.shape[0]:] = Y_train_data
      
+        dset_x = h5f["train_set_x"]
+        dset_y = h5f["train_set_y"]
+        print(np.shape(dset_x))
+        print(np.shape(dset_y))
         
-    with h5py.File('datasets/test_happy.h5', 'a') as h5f:
-        h5f["test_set_x"].resize((hf["test_set_x"].shape[0] + X_test_data.shape[0]), axis = 0)
+    with h5py.File('datasets/test_happy_chunked.h5', 'a') as h5f:
+        h5f["test_set_x"].resize((h5f["test_set_x"].shape[0] + X_test_data.shape[0]), axis = 0)
         h5f["test_set_x"][-X_test_data.shape[0]:] = X_test_data
 
-        h5f["test_set_y"].resize((hf["test_set_y"].shape[0] + Y_test_data.shape[0]), axis = 0)
+        h5f["test_set_y"].resize((h5f["test_set_y"].shape[0] + Y_test_data.shape[0]), axis = 0)
         h5f["test_set_y"][-Y_test_data.shape[0]:] = Y_test_data
- '''   
+        dset_x = h5f["test_set_x"]
+        dset_y = h5f["test_set_y"]
+        print(np.shape(dset_x))
+        print(np.shape(dset_y))
 
-def create_dataset():
-    h5f = h5py.File("datasets/pending_face_test.h5", "w")
+def create_dataset(dataSetFilePath):
+    h5f = h5py.File(dataSetFilePath, "w")
     
     numFile = 1
     pending_set_x = []
@@ -132,10 +195,12 @@ def append_image(filename, label):
 
 #load_dataset()
 #create_dataset()
-
+'''
 n = 4
 for i in range(3,3+n):
     filePath = 'images/happy_'+str(i)+'.jpg'
     print(filePath)
     append_image(filePath, 1)
-#img = image_load('datasets/happy_3.jpg')
+'''
+#copy_dataset()
+update_dataset()
